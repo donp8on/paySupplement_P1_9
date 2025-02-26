d96d34260c1fd513d4845146508d1a5ad263ac2f
donp8on <paytonadonis@gmail.com>
2025-02-26 18:48:23 -0500

    test: created test for InvalidSequence class

--- paySupplement_P1_9Test/InvalidSequenceTest.cs
+++ paySupplement_P1_9Test/InvalidSequenceTest.cs
@@ -1,11 +1,22 @@
+using paySupplement_P1_9;
 namespace paySupplement_P1_9Test
 {
     public class InvalidSequenceTest
     {
         [Fact]
-        public void Test1()
+        public void ShouldTestInvalidSequenceException()
         {
+            var generator = new RandomFloatGenerator();
+            var enumerator = generator.GetEnumerator();
 
+            Assert.Throws<InvalidSequence>(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    enumerator.MoveNext();
+                }
+            });
         }
+
     }
 }
