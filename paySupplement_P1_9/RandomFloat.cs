4f6849529e2c5b0c43d6ca8f97d14a11ae2797fa
donp8on <paytonadonis@gmail.com>
2025-02-27 11:34:45 -0500

    feat: implemented RandomFloatGenerator

--- paySupplement_P1_9/RandomFloat.cs
+++ paySupplement_P1_9/RandomFloat.cs
@@ -6,5 +6,24 @@ namespace paySupplement_P1_9
 {
     public class RandomFloatGenerator : IEnumerable<double>
     {
+        private Random _random = new Random();
+        public IEnumerator<double> GetEnumerator()
+        {
+            int count = 0;
+            while (true)
+            {
+                double value = _random.NextDouble();
+                yield return value;
+
+                if (value <= 0.5)
+                    count++;
+                else
+                    count = 0;
+
+                if (count >= 3)
+                    throw new InvalidSequence("Three consecutive values were ≤ 0.5");
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
