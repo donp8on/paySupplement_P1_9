c53eb0e8ae21e0d6f523e00782c4f3e406e79ae2
donp8on <paytonadonis@gmail.com>
2025-02-27 11:47:52 -0500

    doc: documentation for RandomFloatGenerator

--- paySupplement_P1_9/RandomFloat.cs
+++ paySupplement_P1_9/RandomFloat.cs
@@ -7,6 +7,12 @@ namespace paySupplement_P1_9
     public class RandomFloatGenerator : IEnumerable<double>
     {
         private Random _random = new Random();
+        /// <summary>
+        /// Generates a random float between 0 and 1
+        /// Will throw an exception if three consecutive values are less than or equal to 0.5.
+        /// </summary>
+        /// <returns>Returns a random float between 0.0 ad 1.0.</returns>
+        /// <exception cref="InvalidSequence">This is the error message if three valuse are <= 0.5</exception>
         public IEnumerator<double> GetEnumerator()
         {
             int count = 0;
