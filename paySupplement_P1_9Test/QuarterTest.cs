3492db9116dae8d35d59024da551f979e6c27d07
donp8on <paytonadonis@gmail.com>
2025-02-27 15:13:14 -0500

    test: created Quarter Equality test to check for equality

--- paySupplement_P1_9Test/QuarterTest.cs
+++ paySupplement_P1_9Test/QuarterTest.cs
@@ -4,5 +4,15 @@ namespace paySupplement_P1_9Test
 {
     public class QuarterTest
     {
+        [Fact]
+        public void Quarter_Equality()
+        {
+            var quart1 = new Quarter(0.1);
+            var quart2 = new Quarter(0.2);
+            var quart3 = new Quarter(0.3);
+
+            Assert.True(quart1 == quart2);  
+            Assert.False(quart1 == quart3);
+        }
     }
 }
