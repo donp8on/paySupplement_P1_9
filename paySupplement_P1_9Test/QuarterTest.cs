0065cc1216438ac2dfc9294bc0ee90118d7c4ec0
donp8on <paytonadonis@gmail.com>
2025-02-27 15:37:09 -0500

    test: created Quater Comparison test

--- paySupplement_P1_9Test/QuarterTest.cs
+++ paySupplement_P1_9Test/QuarterTest.cs
@@ -14,5 +14,15 @@ namespace paySupplement_P1_9Test
             Assert.True(quart1 == quart2);  
             Assert.False(quart1 == quart3);
         }
+
+        [Fact]
+        public void QuarterComparison()
+        {
+            var q1 = new Quarter(0.1);
+            var q2 = new Quarter(0.3);
+
+            Assert.True(q1 < q2);
+            Assert.True(q2 > q1);
+        }
     }
 }
