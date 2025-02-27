6f923eccd0ccf5823b2cd9ab32b72f212cace20e
donp8on <paytonadonis@gmail.com>
2025-02-27 11:06:21 -0500

    test: created test for RandomFloat

--- paySupplement_P1_9Test/RandomFloatTest.cs
+++ paySupplement_P1_9Test/RandomFloatTest.cs
@@ -4,5 +4,17 @@ namespace paySupplement_P1_9Test
 {
     public class RandomFloatTest
     {
+        [Fact]
+        public void ShouldTestRandomFloat()
+        {
+            var generator = new RandomFloatGenerator();
+            var enumerator = generator.GetEnumerator();
+
+            for (int i = 0; i < 10; i++)
+            {
+                enumerator.MoveNext();
+                Assert.InRange(enumerator.Current, 0.0, 1.0);
+            }
+        }
     }
 }
