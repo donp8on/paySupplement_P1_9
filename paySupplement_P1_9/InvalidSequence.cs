abdbc11df994cd296ff71dfa10bd86adc7a521d7
donp8on <paytonadonis@gmail.com>
2025-02-27 10:40:30 -0500

    doc: documentation for InvalidSequence exception message

--- paySupplement_P1_9/InvalidSequence.cs
+++ paySupplement_P1_9/InvalidSequence.cs
@@ -2,6 +2,10 @@
 {
     public class InvalidSequence : Exception
     {
+        /// <summary>
+        /// Exception for invalid sequence that outputs an error message.
+        /// </summary>
+        /// <param name="message">This message describes the error.</param>
         public InvalidSequence(string message) : base(message) { }
     }
 }
